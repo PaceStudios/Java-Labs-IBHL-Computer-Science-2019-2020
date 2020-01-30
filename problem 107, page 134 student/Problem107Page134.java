417f006ebf6683870f14f8c9144b7544194237b7
PaceStudios <36940152+PaceStudios@users.noreply.github.com>
2020-01-30 08:54:36 -0800

    pp.134 Problem 107

---
+++ problem 107, page 134 student/Problem107Page134.java
@@ -0,0 +1,33 @@
+import java.util.*;
+import java.lang.Math;
+/**
+ * Friday the 13.
+ *
+ * @author  
+ * @version (a version number or a date)
+ */
+public class Problem107Page134
+{
+/*
+ *  returns an array of all months with a Friday the 13th in Calendar order
+ *        Use the following Strings for each Month
+ *        "Jan", "Feb", "March", "April", "May", "June", "July", "Aug", "Sept", "Oct", "Nov", "Dec"
+ *
+ *  Please note:  The objective of this program is to use the formula presented in the book.
+ *      
+ *  The tester test long ago years (like year 1, 2, etc.) which may differ from actual Friday the 13ths
+ *     
+ *  More on Leap Years (According to wikipedia):
+ *        Every year that is exactly divisible by four is a leap year,
+ *        except for years that are exactly divisible by 100,
+ *        but these centurial years are leap years if they are exactly divisible by 400.
+ *        For example, the years 1700, 1800, and 1900 were not leap years, but the years 1600 and 2000 were
+ */
+   public static String[] findFriday13(int year)
+   {
+      String[] ans = {"Jan", "Feb", "March", "April", "May", "June", "July", "Aug", "Sept", "Oct", "Nov", "Dec"};
+      
+      return ans;
+   }
+
+}
