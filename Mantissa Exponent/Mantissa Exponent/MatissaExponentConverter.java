ef313a2d105f83cc8ff4311966a77073cdd6e10b
PaceStudios <36940152+PaceStudios@users.noreply.github.com>
2020-01-31 14:26:48 -0800

    Working Mantissa (01/31/20)

--- Mantissa Exponent/Mantissa Exponent/MatissaExponentConverter.java
+++ Mantissa Exponent/Mantissa Exponent/MatissaExponentConverter.java
@@ -2,93 +2,127 @@ import java.util.*;
 
 public class MatissaExponentConverter
 {
-    /*
-     *  Convert the parameter val to its m-bit matissa and e-bit exponent
-     *  with one space seperating the matissa and the exponent
-     *  
-     *  return "np" if the conversion is not possible
-     * 
-     * preconditions:  -???? >  val  > ????
-     * 
-     *         see the student tester for sample input/output
-     *         
-     *   if val == 0, return a mantissa = 000...000 and exponent = 000....0
-     */
-    public static String toMatissaExponent(double val, int m, int e)
-    {
-        String expression = "";
-        int matissaLength = m;
-        int exponentLength = e;
-        double manipulatedValue = 0;
-        String valVal = val + "";
-        String leftOfDecimal = "";
-        String rightOfDecimal = "";
-        System.out.println("Length of Double value in Val: " + valVal.length());
+   /*
+    *  Convert the parameter val to its m-bit matissa and e-bit exponent
+    *  with one space seperating the matissa and the exponent
+    *  
+    *  return "np" if the conversion is not possible
+    * 
+    * preconditions:  -???? >  val  > ????
+    * 
+    *         see the student tester for sample input/output
+    *         
+    *   if val == 0, return a mantissa = 000...000 and exponent = 000....0
+    */
+   public static String toMatissaExponent(double val, int m, int e)
+   {
+       String ans=""+val;
+       String exp="";
+       int expNum=0;
+       if(val>0)
+       {
+           expNum=Integer.toBinaryString(Integer.parseInt(ans.substring(0,ans.indexOf(".")))).length();
+           //if(Integer.parseInt(ans.substring(ans.indexOf(".")+1)) != 0)
+               ans=Integer.toBinaryString(Integer.parseInt(ans.substring(0,ans.indexOf("."))))+toDecimalBinary(ans.substring(ans.indexOf(".")+1));
+           /*else
+               ans=Integer.toBinaryString(Integer.parseInt(ans.substring(0,ans.indexOf("."))));*/
+           ans="0"+ans;
+           if(ans.length()>m)
+               return "np";
+           while(ans.length()<m)
+               ans="0"+ans;
+           exp=Integer.toBinaryString(expNum);
+           if(exp.length()>e)
+               return "np";
+           while(exp.length()<e)
+               exp="0"+exp;
+       }
+       else if(val<0)
+       {
+           ans=""+Math.abs(val);
+           expNum=Integer.toBinaryString(Integer.parseInt(ans.substring(0,ans.indexOf(".")))).length();
+           ans=Integer.toBinaryString(Integer.parseInt(ans.substring(0,ans.indexOf("."))))+toDecimalBinary(ans.substring(ans.indexOf(".")+1));
+           ans="0"+ans;
+           String temp="";
+           for(int x=0; x<ans.length(); x++)
+           {
+               if(ans.substring(x,x+1).equals("1"))
+                   temp+="0";
+               else
+                   temp+="1";
+           }
+           ans=Integer.toBinaryString((int)toDouble(temp)+1);
+           if(ans.length()>m)
+               return "np";
+           while(ans.length()<m)
+               ans="0"+ans;
+           exp=Integer.toBinaryString(expNum);
+           if(exp.length()>e)
+               return "np";
+           while(exp.length()<e)
+               exp="0"+exp;
+       }
+       else if(val==0)
+       {
+           for(int x=0; x<m; x++)
+               ans+="0";
+           for(int x=0; x<e; x++)
+               exp+="0";
+       }
+       
+       return ans + " " + exp;
+   }
 
-        System.out.println("The number 0.625 converted to Binary is: " + convertRealNumbers(0.625));
-        int i = 0;
-        while(!valVal.substring(i,i+1).equals(".")){
-            leftOfDecimal += valVal.substring(i,i+1);
-            i++;
-        }
-        System.out.println("Value of I is:" + i);
-        System.out.println("The length of the Original String is: " + valVal.length());
-        rightOfDecimal = valVal.substring(i, valVal.length());
-        System.out.println("Value Right of the Decimal before Conversion = " + rightOfDecimal);
-        rightOfDecimal = convertRealNumbers(Integer.parseInt(rightOfDecimal));
-        System.out.println("Value Right of the Decimal following conversion: " + rightOfDecimal);
-        System.out.println("Value Currently left of the Decimal: " + leftOfDecimal);
-        System.out.println("Print Expression: " + leftOfDecimal);
-
-        return expression;
-    }
-
-    public double convertNegative(double friend){
-        return -1;
-    }
-
-    public static String convertRealNumbers(double num){
-        if (num >= 1 || num <= 0) 
-            return "ERROR"; 
-
-        StringBuilder binary = new StringBuilder(); 
-        binary.append("."); 
-
-        while (num > 0) 
-        { 
-            /* Setting a limit on length: 32 characters, 
-            If the number cannot be represented 
-            accurately in binary with at most 32 
-            character  */
-            if (binary.length() >= 32) 
-                return "ERROR"; 
-
-            // Multiply by 2 in num to check it 1 or 0 
-            double r = num * 2; 
-            if (r >= 1) 
-            { 
-                binary.append(1); 
-                num = r - 1; 
-            } 
-            else
-            { 
-                binary.append(0); 
-                num = r; 
-            } 
-        } 
-        return binary.toString(); 
-    }
-
-    /*
-     *    converts the mantissa-exponent string to its equivalent real number
-     *    
-     *    precondition: str contains one space seperating the matissa from the exponent 
-     *
-     * 
-     *         see the student tester for sample input/output
-     */
-    public static double toDouble(String str)
-    {
-        return Math.random();
-    }
+   /*
+    *    converts the mantissa-exponent string to its equivalent real number
+    *    
+    *    precondition: str contains one space seperating the matissa from the exponent 
+    *t
+    * 
+    *         see the student tester for sample input/output
+    */
+   public static double toDouble(String str)
+   {
+       double mantissa=0;
+       double exp=0;
+       for(int x=0; x<str.indexOf(" "); x++)
+       {
+           if(str.substring(x,x+1).equals("1") && x==0)
+               mantissa-=1;
+           else if(str.substring(x,x+1).equals("1"))
+               mantissa+=Math.pow(2,-x);
+       }
+       str=str.substring(str.indexOf(" ")+1);
+       for(int x=0; x<str.length(); x++)
+       {
+           if(str.substring(x,x+1).equals("1") && x==0)
+               exp+= -Math.pow(2,str.length()-1);
+           else if(str.substring(x,x+1).equals("1"))
+               exp+=Math.pow(2,str.length()-1-x);
+       }
+       return mantissa*Math.pow(2,exp);
+   }
+   
+   
+   public static String toDecimalBinary(String num)
+   {
+       String ans="";
+       int temp=Integer.parseInt(num);
+       int count=(int)Math.pow(2, -num.length());
+       Stack<Integer> s=new Stack<Integer>();
+       for(int x=count; x>=0; x-=Math.pow(2, -x))
+       {
+           if(temp-Math.pow(2, -x)>=0)
+           {
+               temp-=Math.pow(2, -x);
+               s.push(1);
+           }
+           else
+               s.push(0);
+       }
+       while(s.size()>0)
+           ans+=s.pop();
+       return ans;
+   }
+   
 }
