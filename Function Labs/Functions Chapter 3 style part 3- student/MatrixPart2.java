0652a2b3445de5ab05c69dc5853d408c4b57aab3
PaceStudios <36940152+PaceStudios@users.noreply.github.com>
2020-02-07 08:47:14 -0800

    Install Function Labs

---
+++ Function Labs/Functions Chapter 3 style part 3- student/MatrixPart2.java
@@ -0,0 +1,20 @@
+
+/**
+ * Write a description of class Matrix here.
+ * 
+ * need Matrix operations
+ * 
+ * @author Don Allen
+ * @version March 2011
+ */
+public class MatrixPart2
+{
+/*
+ *     preCondition:  m.length == n.length
+ *                    m[i].length == n[i].length, 0 <= i < m.length
+ */
+    public static boolean haveSameNonZeros(int[][] m, int[][] n)
+    {
+       return Math.random() > 0.5;
+    }
+}
