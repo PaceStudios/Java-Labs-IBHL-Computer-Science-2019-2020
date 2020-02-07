e1e36b8fb27d3fd81fda13ac9de888128e6cfe3a
PaceStudios2012@gamil.com <pacestudios2012@gmail.com>
2020-02-07 05:46:42 -0800

    PatchedMantissa

--- Mantissa Exponent/Mantissa Exponent/MatissaExponentConverter.java
+++ Mantissa Exponent/Mantissa Exponent/MatissaExponentConverter.java
@@ -4,7 +4,7 @@ import java.math.*;
 public class MatissaExponentConverter
 {
     /*
-     *  Convert the parameter val to its m-bit matissa and e-bit exponent
+     *  Convert the paramyStringter val to its m-bit matissa and e-bit exponent
      *  with one space seperating the matissa and the exponent
      *  
      *  return "np" if the conversion is not possible
@@ -15,7 +15,7 @@ public class MatissaExponentConverter
      *         
      *   if val == 0, return a mantDigit = 000...000 and exponent = 000....0
      */
-    public static String toMatissaExponent(double val, int m, int e)
+    public static String toMatissaExponent2(double val, int m, int e)
     {
         String built = convertToBinary(val, m, e);
         System.out.println("A");
@@ -124,7 +124,7 @@ public class MatissaExponentConverter
      * 
      *         see the student tester for sample input/output
      */
-    public static double toDouble(String str)
+    public static double toDouble2(String str)
     {
         double mantDigit=0;
         double exp=0;
@@ -146,17 +146,17 @@ public class MatissaExponentConverter
         return mantDigit*Math.pow(2,exp);
     }
 
-    public static String toDecimalBinary(String num)
+    public static String toDecimalBinary2(String num)
     {
         String ans="";
-        int temp=Integer.parseInt(num);
+        int tempVal=Integer.parseInt(num);
         int count=(int)Math.pow(2, -num.length());
         Stack<Integer> s=new Stack<Integer>();
         for(int x=count; x>=0; x-=Math.pow(2, -x))
         {
-            if(temp-Math.pow(2, -x)>=0)
+            if(tempVal-Math.pow(2, -x)>=0)
             {
-                temp-=Math.pow(2, -x);
+                tempVal-=Math.pow(2, -x);
                 s.push(1);
             }
             else
@@ -167,4 +167,104 @@ public class MatissaExponentConverter
         return ans;
     }
 
+    public static String toMatissaExponent(double val, int mBits, int eBits)
+    {
+        String myString = "";
+
+
+        if(val == 0.0)
+        {
+            for(int i=0; i<mBits; i++)
+            {
+                myString += "0";
+            }
+            myString += " ";
+            for(int i=0; i<eBits; i++)
+            {
+                myString += "0";
+            }
+            return myString;
+        }
+
+        //convert on the left side of the decimal
+        //is it negative or not
+        double exp = Math.floor((Math.log(Math.abs(val))/Math.log(2)) + 1);
+        val /= Math.pow(2, exp);
+        if(val < 0)
+        {
+            val = 1 - Math.abs(val);
+            myString += "1";
+        }
+        else
+        {
+            myString += "0";
+        }
+
+        double tempVal = 0.5;
+        for(int i=1; i<mBits; i++)
+        {
+            if(val == 0.0)
+                myString += "0";
+            else
+            {
+                if((val-tempVal)>=0){
+                    myString += "1";
+                    val -= tempVal;
+                }else
+                    myString += "0";
+            }
+            tempVal /= 2;
+        }
+
+        if(val!=0)
+        {
+            return "np";
+        }
+        myString += " ";
+        tempVal = Math.pow(2, eBits-1);
+        if(exp < 0)
+        {
+            exp = tempVal + exp;
+            myString += "1";
+        }
+        else
+            myString += "0";
+        for(int i=1; i<eBits; i++)
+        {
+            tempVal /= 2;
+            if(exp == 0.0)
+                myString += "0";
+            else
+            {
+                if((exp-tempVal)>=0){
+                    myString += "1";
+                    exp -= tempVal;
+                }else
+                    myString += "0";
+            }
+        }
+        if(exp!=0){
+            return "np";
+        }
+        return myString;
+    }
+
+    /**
+     * Converts mantissa exponent in string form separated by one space into a decimal in double form
+     */
+    public static double toDouble(String str)
+    {
+        String[] myString = str.split(" ");
+        double ans = -1 * Double.parseDouble(myString[0].substring(0, 1));
+        for(int i=1; i<myString[0].length(); i++)
+        {
+            ans += Double.parseDouble(myString[0].substring(i, i+1))*Math.pow(2, -1*i);
+        }
+        double exp = -1*Double.parseDouble(myString[1].substring(0, 1))*Math.pow(2, myString[1].length()-1);
+        for(int j=1; j<myString[1].length(); j++)
+        {
+            exp += Double.parseDouble(myString[1].substring(j, j+1))*Math.pow(2, myString[1].length()-j-1);
+        }
+        return ans * Math.pow(2, exp);
+    }
 }
